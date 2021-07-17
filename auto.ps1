545ea4d40f2815add2061ec84c33457dd3a3959b
sergej <sergej>
2021-07-17 13:49:03 +0200

    07/17/2021 13:49:03

--- auto.ps1
+++ auto.ps1
@@ -3,8 +3,8 @@ while($true)
     $date = Get-Date;
     Write-Host $date;
     git add .;
-    git commit -m "$date";
-    git push
+    git commit -m "$date" -q
+    git push -q
     start-sleep -seconds 60;
     
 }
